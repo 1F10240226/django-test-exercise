68e96b14d044d5605b701a3601bc766c8e2afe6a
1F10240226 <s1f102402260@iniad.org>
2025-07-03 13:58:18 +0900

    flake8 2nd revised

--- todo/tests.py
+++ todo/tests.py
@@ -73,7 +73,7 @@ class TodoViewTestCase(TestCase):
     def test_index_get_order_post(self):
         task1 = Task(title="task1", due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()
-        task2 = Task(title= "task2", due_at=timezone.make_aware(datetime(2024, 8, 1)))
+        task2 = Task(title="task2", due_at=timezone.make_aware(datetime(2024, 8, 1)))
         task2.save()
         client = Client()
         response = client.get("/?order=post")
@@ -95,4 +95,3 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, "todo/index.html")
         self.assertEqual(response.context["tasks"][0], task1)
         self.assertEqual(response.context["tasks"][1], task2)
-    
